90242ce58a87e8af2934470159fca2e5fa41504f
dio <abrahamdio@gmail.com>
2017-09-22 02:14:46 -0700

    final

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,23 +1,31 @@
-import React from 'react'
 import {connect} from 'react-redux'
-import {updateCurrent} from '../reducers/todo'
+import React, {Component} from 'react'
+import {updateCurrent, saveTodo} from '../reducers/todo'
 
-const TodoForm = (props) => {
-    const {currentTodo, updateCurrent} = props
-    const handleInputChange = (evt) => {
+class TodoForm extends Component {
+    handleInputChange = (evt) => {
         const val = evt.target.value;
-        updateCurrent(val)
+        this.props.updateCurrent(val)
     }
-    return (
-        <form>
-            <input type="text" 
-                onChange={handleInputChange}
-                value={currentTodo}/>
-         </form>
-    )
+
+    handleSubmit = (evt) => {
+        evt.preventDefault()
+        this.props.saveTodo(this.props.currentTodo)
+    }
+
+    render() {
+        const {currentTodo} = this.props
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" 
+                    onChange={this.handleInputChange}
+                    value={currentTodo}/>
+             </form>
+        )
+    }
 }
 
 export default connect(
-    (state) => ({currentTodo: state.currentTodo}),
-    {updateCurrent}
+    (state) => ({currentTodo: state.todo.currentTodo}), // state(s) that will be available on props in component
+    {updateCurrent, saveTodo} // function(s) that will be available on props in component
 )(TodoForm)
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {fetchTodos, toggleTodo, deleteTodo, getVisibleTodos} from '../reducers/todo'
 
-const TodoItem = ({id, name, isComplete}) => (
+const TodoItem = ({id, name, isComplete, toggleTodo, deleteTodo}) => (
     <li>
-        <input type="checkbox" defaultChecked={isComplete}/>
+        <span className="delete-item">
+            <button onClick={() => deleteTodo(id)}>X</button>
+        </span>
+        <input type="checkbox" checked={isComplete} onChange={() => toggleTodo(id)}/>
         {name}
     </li>   
 )
 
-const TodoList = (props) => (
-    <ul>
-        {props.todos.map(todo => (
-            <TodoItem key={todo.id} {...todo} />
-        ))}
-    </ul>    
-)
+class TodoList extends Component {
+    componentDidMount() {
+        this.props.fetchTodos()
+    }
+    render() {
+        return (
+            <div className="Todo-List">
+                <ul>
+                    {this.props.todos.map(todo => (
+                        <TodoItem key={todo.id} 
+                        toggleTodo={this.props.toggleTodo}
+                        deleteTodo={this.props.deleteTodo} 
+                        {...todo} />
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
 
 export default connect(
-    (state) => ({todos: state.todos})
+    (state, ownProps) => ({todos: getVisibleTodos(state.todo.todos, ownProps.filter)}), // MapStateToProps
+    {fetchTodos, toggleTodo, deleteTodo} // MapDispatchToProps
 )(TodoList)
